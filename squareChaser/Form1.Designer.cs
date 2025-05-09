5faf139bb5142f667875ad295276bf09ef1b26a6
henrflan986 <henrflan986@SDS-L-262PZP>
2025-05-09 14:27:21 -0400

    Add project files.

---
+++ squareChaser/Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace squareChaser
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.gameEngine = new System.Windows.Forms.Timer(this.components);
+            this.winLabel = new System.Windows.Forms.Label();
+            this.freezeTimer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // gameEngine
+            // 
+            this.gameEngine.Enabled = true;
+            this.gameEngine.Interval = 20;
+            this.gameEngine.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // winLabel
+            // 
+            this.winLabel.AutoSize = true;
+            this.winLabel.Location = new System.Drawing.Point(344, 56);
+            this.winLabel.Name = "winLabel";
+            this.winLabel.Size = new System.Drawing.Size(0, 13);
+            this.winLabel.TabIndex = 0;
+            // 
+            // freezeTimer
+            // 
+            this.freezeTimer.Interval = 3000;
+            this.freezeTimer.Tick += new System.EventHandler(this.freezeTimer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 561);
+            this.Controls.Add(this.winLabel);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer gameEngine;
+        private System.Windows.Forms.Label winLabel;
+        private System.Windows.Forms.Timer freezeTimer;
+    }
+}
+
---
+++ squareChaser/Form1.cs
@@ -0,0 +1,279 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+using System.Threading;
+
+namespace squareChaser
+{
+    public partial class Form1 : Form
+    {
+
+        Random randGen = new Random();
+        SoundPlayer speedBoost = new SoundPlayer(Properties.Resources.speedSound);
+        SoundPlayer winner = new SoundPlayer(Properties.Resources.winnerSound);
+
+
+        int xValue;
+        Rectangle player1 = new Rectangle(200, 200, 15, 15);
+        Rectangle player2 = new Rectangle(250, 200, 15, 15);
+        Rectangle ball = new Rectangle(295, 195, 15, 15);
+        Rectangle speedball = new Rectangle(400, 195, 15, 15);
+        Rectangle freezeBall = new Rectangle( 200, 300, 10, 10);
+        Rectangle border = new Rectangle(100, 100, 400, 400);
+        Rectangle filler = new Rectangle(125, 125, 350, 350);
+       
+
+        int player1Score = 0;
+        int player2Score = 0;
+
+       // int playerSpeed = 3;
+        int player1Speed = 3;
+        int player2Speed = 3;
+
+        bool wDown = false;
+        bool sDown = false;
+        bool upArrowDown = false;
+        bool downArrowDown = false;
+        bool aLeft = false;
+        bool dRight = false;
+        bool leftArrow = false;
+        bool rightArrow = false;
+
+        SolidBrush blueBrush = new SolidBrush(Color.DodgerBlue);
+        SolidBrush whiteBrush = new SolidBrush(Color.White);
+        SolidBrush blackBrush = new SolidBrush(Color.Black);
+        SolidBrush greenBrush = new SolidBrush(Color.Green);
+        SolidBrush redBrush = new SolidBrush(Color.Red);
+        SolidBrush purpleBrush = new SolidBrush(Color.Purple);
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.W:
+                    wDown = true;
+                    break;
+                case Keys.S:
+                    sDown = true;
+                    break;
+                case Keys.A:
+                    aLeft = true;
+                    break;
+                case Keys.D:
+                    dRight = true;
+                    break;
+                case Keys.Up:
+                    upArrowDown = true;
+                    break;
+                case Keys.Down:
+                    downArrowDown = true;
+                    break;
+                case Keys.Right:
+                    leftArrow = true;
+                    break;
+                case Keys.Left:
+                    rightArrow = true;
+                    break;
+            }
+
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.W:
+                    wDown = false;
+                    break;
+                case Keys.S:
+                    sDown = false;
+                    break;
+                case Keys.A:
+                    aLeft = false;
+                    break;
+                case Keys.D:
+                    dRight = false;
+                    break;
+                case Keys.Up:
+                    upArrowDown = false;
+                    break;
+                case Keys.Down:
+                    downArrowDown = false;
+                    break;
+                case Keys.Right:
+                    leftArrow = false;
+                    break;
+                case Keys.Left:
+                    rightArrow = false;
+                    break;
+            }
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.FillRectangle(blackBrush, border);
+            e.Graphics.FillRectangle(whiteBrush, filler);
+            e.Graphics.FillRectangle(redBrush, player1);
+            e.Graphics.FillRectangle(blueBrush, player2);
+            e.Graphics.FillRectangle(greenBrush, ball);
+            e.Graphics.FillRectangle(blackBrush, speedball);
+            e.Graphics.FillEllipse(purpleBrush, freezeBall);
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+        
+
+            //move player 1
+            if (wDown == true && player1.Y > 125)
+            {
+                player1.Y -= player1Speed;
+            }
+
+            if (sDown == true && player1.Y < 475 - player1.Width)
+            {
+                player1.Y += player1Speed;
+            }
+            if (aLeft == true && player1.X > 125)
+            {
+                player1.X -= player1Speed;
+            }
+
+            if (dRight == true && player1.X < 475 - player1.Width)
+            {
+                player1.X += player1Speed;
+            }
+            //move player 2
+            if (upArrowDown == true && player2.Y > 125)
+            {
+                player2.Y -= player2Speed;
+            }
+
+            if (downArrowDown == true && player2.Y < 475 - player2.Height)
+            {
+                player2.Y += player2Speed;
+            }
+            if (rightArrow == true && player2.X > 125)
+            {
+                player2.X -= player2Speed;
+            }
+
+            if (leftArrow == true && player2.X < 475 - player2.Width)
+            {
+                player2.X += player2Speed;
+            }
+
+            //check if ball hits top wall or the bottom wall
+          
+            //check if ball hits either player.
+            //If it does change the direction
+            //and place the ball in front of the player hit
+            if (player1.IntersectsWith(ball))
+            {
+                ball.X = randGen.Next(125, 475);
+                ball.Y = randGen.Next(125, 475);
+                player1Score++;
+                winner.Play();
+            }
+            else if (player2.IntersectsWith(ball))
+            {
+                ball.X = randGen.Next(125, 475);
+                ball.Y = randGen.Next(125, 475);
+                player2Score++;
+                winner.Play();
+            }
+            if (player1.IntersectsWith(speedball))
+            {
+                speedball.X = randGen.Next(125, 475);
+                speedball.Y = randGen.Next(125, 475);
+                player1Speed += 2;
+                speedBoost.Play();
+            }
+            else if (player2.IntersectsWith(speedball))
+            {
+                speedball.X = randGen.Next(125, 475);
+                speedball.Y = randGen.Next(125, 475);
+                player2Speed++;
+                player2Speed++;
+                speedBoost.Play();
+            }
+            if (player1.IntersectsWith(freezeBall))
+            {
+                freezeBall.X = randGen.Next(125, 475);
+                freezeBall.Y = randGen.Next(125, 475);
+
+                player2Speed = 0;
+                freezeTimer.Enabled = true;
+
+
+
+
+            }
+            else if (player2.IntersectsWith(freezeBall))
+            {
+                freezeBall.X = randGen.Next(125, 475);
+                freezeBall.Y = randGen.Next(125, 475);
+
+                player1Speed = 0;
+               freezeTimer.Enabled = true;
+
+            }
+
+            //if (player1.IntersectsWith(bombBall))
+            //{
+            //    bombBall.X = randGen.Next(125, 475);
+            //    bombBall.Y = randGen.Next(125, 475);
+
+            //    player2Speed = 0;
+            //    freezeTimer.Enabled = true;
+
+
+
+
+            //}
+            //else if (player2.IntersectsWith(bombBall))
+            //{
+            //    bombBall.X = randGen.Next(125, 475);
+            //    bombBall.Y = randGen.Next(125, 475);
+
+            //    player1Speed = 0;
+            //    freezeTimer.Enabled = true;
+
+                //check if game is over
+                if (player1Score == 5)
+            {
+                winLabel.Visible = true;
+                winLabel.Text = "Player 1 wins!!";
+                gameEngine.Enabled = false;
+            }
+            else if (player2Score == 5)
+            {
+                gameEngine.Enabled = false;
+                winLabel.Visible = true;
+                winLabel.Text = "Player 2 wins!!";
+            }
+
+            Refresh(); // runs the Paint method
+        }
+
+        private void freezeTimer_Tick(object sender, EventArgs e)
+        {
+            freezeTimer.Enabled = false;
+            player2Speed = 3;
+            player1Speed = 3;
+        }
+    }
+}
+
+
